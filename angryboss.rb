468dffdd7170f4ba6eb640b9a54af63afbfe2456
Julian Rockwood <julianjamesrockwood@gmail.com>
2014-04-12 16:26:46 -0500

    angry_boss 1.0

---
+++ angryboss.rb
@@ -0,0 +1,6 @@
+# write a program where your boss asks you what you want.
+# after you type in your answer the boss should yell at you and then fire you.
+
+puts 'I dont have a lot of time. What is it that you want?'
+request = gets.chomp
+puts 'WHAT THE FUCK DO YOU MEAN YOU WANT  ' + request + '!!!? YOURE FIRED!'
